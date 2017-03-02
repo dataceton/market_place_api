d0072a83f8063ce0ab31d84c623b353ce3853031
Daniil Davydov <nexusimagination@gmail.com>
2017-03-02 18:05:24 +0300

    Initializes the project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MarketPlaceApi::Application.config.secret_token = 'cb54fcc38ba744a2c225b00a9dfbb327d3d895fd0a7e8052741d81581487261e2f1e177b3861d22f1b4d8b81291af0b979e89c9ee4eee22493569ff447634439'
